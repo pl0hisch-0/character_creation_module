ac6f5168ea0210fe4b3c81062d52dad7f9460e3e
Серафим <s_sir_sima@mail.ru>
2023-03-24 15:17:04 +0300

    code according inicialisation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный {5+randint(3, 5)}')
     if char_class == 'mage':
@@ -11,7 +11,7 @@ def attack(char_name, char_class):
     return (f'{char_name} не нанес урона')
 
 
-def defence(char_name, char_class):
+def defence(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -21,7 +21,7 @@ def defence(char_name, char_class):
     return ('не заблокировал удар')
 
 
-def special(char_name, char_class):
+def special(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 '«Выносливость {80 + 25}»')
@@ -32,7 +32,7 @@ def special(char_name, char_class):
     return (f'{char_name} не применил спеуиального умения')
 
 
-def start_training(char_name, char_class):
+def start_training(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     elif char_class == 'mage':
